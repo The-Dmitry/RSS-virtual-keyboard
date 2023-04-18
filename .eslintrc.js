626c36697d9a461526fa9cb882d1a6e95a6639e2
the-dmitry <dzmitrygaponenko@gmail.com>
2023-04-18 21:29:23 +0300

    feat: configure webpack and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'airbnb-base/legacy',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    // 'import/extensions': 'off'
+    'linebreak-style': [
+      'error',
+      'windows'
+    ]
+  }
+};
